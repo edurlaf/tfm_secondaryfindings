429112608eafeb21d0541261bf9ce5b1ce201113
edurlaf <edurlaf@gmail.com>
2023-10-10 00:03:15 +0200

    assign_dpyd_diplotype function added

--- 05c_run_fg_module.py
+++ 05c_run_fg_module.py
@@ -10,7 +10,7 @@ import pysam
 import csv
 
 # Definir las funciones para cada módulo y opción
-def annotate_fg_variants(config, output_file, assembly):
+def annotate_fg_variants(dir_path, vcf_path, output_file, assembly):
     """
     Ejecuta el programa Intervar para anotar variantes genéticas.
     
@@ -24,16 +24,16 @@ def annotate_fg_variants(config, output_file, assembly):
         subprocess.CalledProcessError: Si hay un error al ejecutar Intervar.
     """
     # Cargar archivo fg_json con variantes farmacogenéticas
-    fg_json_path = config['dir_path'] + 'fg_risk_variants_' + assembly + '.json'
+    fg_json_path = dir_path + 'fg_risk_variants_' + assembly + '.json'
     with open(fg_json_path, 'r') as file:
         fg_json = json.load(file)
 
     annotated_variants = []
 
     # Abrir el archivo VCF
-    vcf_path = config['vcf_path'].split('.vcf')[0] + '_fg_intersection.vcf' #cambiarlo, vcfpath no va a estar en config
+    vcf_int_path = vcf_path.split('.vcf')[0] + '_fg_intersection.vcf'
     
-    with open(vcf_path, 'r') as vcf_file:
+    with open(vcf_int_path, 'r') as vcf_file:
     #with pysam.VariantFile(vcf_path) as vcf_file:
         for line in vcf_file:
             fields = line.split('\t')
@@ -161,7 +161,89 @@ def assign_cyp2c19_diplotype(variants):
             else:
                 print('Se han encontrado variantes en CYP2C19 no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
                 
-    return(diplotype) 
+    return(diplotype)
+
+def assign_dpyd_diplotype(variants):
+    # Gen
+    gene = 'DPYD'
+    
+    # Flag para verificar si se encontró una variante en el gen objetivo
+    found = False
+    
+    variants_gene = {}
+    # Iterar a través de las variantes
+    for variant in variants:
+        if variant['Gene'] == gene:
+            found = True
+            variants_gene[variant['rs']] = variant['GT']
+            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+            
+    # Si no se encontró ninguna variante, diplotipo *1/*1
+    if found == False:
+        diplotype = '*1/*1'
+        
+    else:
+        # Chequear *2A:
+        if 'rs3918290' in variants_gene.keys():
+            if variants_gene['rs3918290'] == '1/1': # en realidad habría que comprobar si es len >1, porque puede que esté en fase con otro alelo
+                diplotype = '*2A/*2A'
+            else:
+                if len(variants_gene) == 1:
+                    diplotype = '*1/*2A'
+                elif len(variants_gene) == 2:
+                    if 'rs55886062' in variants_gene.keys():
+                        diplotype = '*2A/*13'
+                    elif 'rs67376798' in variants_gene.keys():
+                        diplotype = '*2A/c.2846A>T'
+                    else:
+                        print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+                else:
+                    if 'rs75017182' in variants_gene.keys() and 'rs56038477' in variants_gene.keys():
+                        diplotype = '*2a/HapB3'
+                    else:
+                        print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+
+        # Chequear *13:
+        elif 'rs55886062' in variants_gene.keys():
+            if variants_gene['rs55886062'] == '1/1':
+                diplotype = '*13/*13'
+            else:
+                if len(variants_gene) == 1:
+                    diplotype = '*1/*13'
+                elif len(variants_gene) == 2:
+                    elif 'rs67376798' in variants_gene.keys():
+                        diplotype = '*13/c.2846A>T'
+                    else:
+                        print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+                else:
+                    if 'rs75017182' in variants_gene.keys() and 'rs56038477' in variants_gene.keys():
+                        diplotype = '*13/HapB3'
+                    else:
+                        print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+        # Chequear c.2846A>T:
+        elif 'rs67376798' in variants_gene.keys():
+            if variants_gene['rs67376798'] == '1/1':
+                diplotype = 'c.2846A>T/c.2846A>T'
+            else:
+                if len(variants_gene) == 1:
+                    diplotype = '*1/c.2846A>T'
+                elif len(variants_gene) == 2:
+                    print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+                else:
+                    if 'rs75017182' in variants_gene.keys() and 'rs56038477' in variants_gene.keys():
+                        diplotype = 'c.2846A>T/HapB3'
+                    else:
+                        print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+        # Chequear c.2846A>T:
+        elif 'rs75017182' in variants_gene.keys() and 'rs56038477' in variants_gene.keys():
+            if variants_gene['rs75017182'] == '1/1' and variants_gene['rs56038477'] == '1/1':
+                diplotype = 'HapB3/HapB3'
+            else:
+                if len(variants_gene) == 2:
+                    diplotype = '*1/HapB3'
+                else len(variants_gene) == 3:
+                    print('Se han encontrado variantes en DPYD no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
+    return(diplotype)  
         
 def combine_results(vcf_norm, category, intervar_results, clinvar_dct):
     """
