8f10206a2f944775bac785ec9c28069b35166e4a
edurlaf <edurlaf@gmail.com>
2023-10-11 23:37:12 +0200

    run_pharmacogenomic_risk_moduled updated

--- 05c_run_fg_module.py
+++ 05c_run_fg_module.py
@@ -65,20 +65,26 @@ def annotate_fg_variants(dir_path, vcf_path, assembly):
 
     return annotated_variants
 
-def assign_cyp2c9_diplotype(variants):
-    # Gen
-    gene = 'CYP2C9'
-    
+def check_gene_variants(variants, gene):
     # Flag para verificar si se encontró una variante en el gen objetivo
     found = False
-    
+    # Crear diccionario para almacenar genotipo de variantes del gene       #igual mejor guardar la variante también, para el caso de indels con mismo rs y diferente nº de repeticiones
     variants_gene = {}
+    
     # Iterar a través de las variantes
     for variant in variants:
         if variant['Gene'] == gene:
             found = True
             variants_gene[variant['rs']] = variant['GT']
-            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+    
+    return(found, variants_gene)
+
+def assign_cyp2c9_diplotype(variants):
+    # Gen
+    gene = 'CYP2C9'
+    
+    # Comprobar variantes presentes en gen
+    found, variants_gene = check_gene_variants(variants, gene)
             
     # Si no se encontró ninguna variante, diplotipo *1/*1
     if found == False:
@@ -111,16 +117,8 @@ def assign_cyp2c19_diplotype(variants):
     # Gen
     gene = 'CYP2C19'
     
-    # Flag para verificar si se encontró una variante en el gen objetivo
-    found = False
-    
-    variants_gene = {}
-    # Iterar a través de las variantes
-    for variant in variants:
-        if variant['Gene'] == gene:
-            found = True
-            variants_gene[variant['rs']] = variant['GT']
-            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+    # Comprobar variantes presentes en gen
+    found, variants_gene = check_gene_variants(variants, gene)
             
     # Si no se encontró ninguna variante, diplotipo *1/*1
     if found == False or (len(variants_gene) == 1 and 'rs3758581' in variants_gene.keys()):
@@ -167,16 +165,8 @@ def assign_dpyd_diplotype(variants):
     # Gen
     gene = 'DPYD'
     
-    # Flag para verificar si se encontró una variante en el gen objetivo
-    found = False
-    
-    variants_gene = {}
-    # Iterar a través de las variantes
-    for variant in variants:
-        if variant['Gene'] == gene:
-            found = True
-            variants_gene[variant['rs']] = variant['GT']
-            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+    # Comprobar variantes presentes en gen
+    found, variants_gene = check_gene_variants(variants, gene)
             
     # Si no se encontró ninguna variante, diplotipo *1/*1
     if found == False:
@@ -249,16 +239,8 @@ def assign_nudt15_diplotype(variants):
     # Gen
     gene = 'NUDT15'
     
-    # Flag para verificar si se encontró una variante en el gen objetivo
-    found = False
-    
-    variants_gene = {}
-    # Iterar a través de las variantes
-    for variant in variants:
-        if variant['Gene'] == gene:
-            found = True
-            variants_gene[variant['rs']] = variant['GT']
-            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+    # Comprobar variantes presentes en gen
+    found, variants_gene = check_gene_variants(variants, gene)
             
     # Si no se encontró ninguna variante, diplotipo *1/*1
     if found == False:
@@ -294,16 +276,8 @@ def assign_tpmt_diplotype(variants):
     # Gen
     gene = 'TPMT'
     
-    # Flag para verificar si se encontró una variante en el gen objetivo
-    found = False
-    
-    variants_gene = {}
-    # Iterar a través de las variantes
-    for variant in variants:
-        if variant['Gene'] == gene:
-            found = True
-            variants_gene[variant['rs']] = variant['GT']
-            #break  # Se encontró una variante en el gen objetivo, así que puedes salir del bucle
+    # Comprobar variantes presentes en gen
+    found, variants_gene = check_gene_variants(variants, gene)
             
     # Si no se encontró ninguna variante, diplotipo *1/*1
     if found == False:
@@ -407,144 +381,23 @@ def assign_tpmt_diplotype(variants):
                 print('Se han encontrado variantes en TPMT no consideradas en la asignación de haplotipos en esta herramienta. Revisar manualmente.')
     return(diplotype)  
        
-def combine_results(vcf_norm, category, intervar_results, clinvar_dct):
+
+def run_pharmacogenomic_risk_module(dir_path, vcf_path, assembly): #sobra category, este modulo es especifico de rr
     """
-    Combina los resultados de Intervar y ClinVar en una sola línea por variante.
-
-    Args:
-        vcf_norm (str): Ruta al archivo VCF normalizado.
-        category (str): Categoría de genes para la anotación.
-        intervar_results (dict): Resultados de Intervar.
-        clinvar_dct (dict): Base de datos de ClinVar.
-
-    Returns:
-        dict: Un diccionario con los resultados combinados.
-    """
-    combined_results = {}
-    
-    # para compbinar los resultados de intervar y clinvar, aunque lo ideal sería recorrer
-    # las listas, intervar cambia la anotación, eliminando el nt de referencia en las idnels
-    # por lo que no es posible encontrar esa variante en clinvar (no siempre hay rs disponible)
-    # así que lo único que se me ocurre es recorrer el vcf de interseccion
-    
-
-
-    # Archivo VCF de intersección
-    vcf_path = vcf_norm.split('normalized')[0] + category + '_intersection.vcf'
-    
-    try:
-        # Abrir el archivo VCF de intersección
-        #with pysam.VariantFile(vcf_path, "r") as vcf_file:
-        with open(vcf_path, "r") as vcf_file:
-            #for variant in vcf_file:
-            # Recorrer el archivo línea por línea
-            for line in vcf_file:
-                fields = line.strip().split("\t")
-                chrom = fields[0]
-                pos = fields[1]
-                ref = fields[3]
-                alt = fields[4]  # Supongo una sola alternativa porque he splitteado los multiallelic sites
-        
-                # Construye la clave de búsqueda
-                variant_key = f"{chrom}:{pos}:{ref}:{alt}"
-        
-                # Busca la variante en los resultados de Intervar
-                if len(ref) > len(alt):
-                    if len(alt) == 1:
-                        #variant_int = chrom + ':' + str(int(pos) + 1) + ':' + ref[1:] + ':-'
-                        variant_int = f"{chrom}:{int(pos) + 1}:{ref[1:]}:-"
-                    else:
-                        variant_int = chrom + ':' + str(int(pos) + 1) + ':' + ref[1:] + ':' + alt[1:]
-                else:
-                    variant_int = f"{chrom}:{pos}:{ref}:{alt}"
-                        
-                intervar_info = intervar_results.get(variant_int)
-        
-                # Busca la variante en los resultados de ClinVar
-                clinvar_info = clinvar_dct.get(variant_key)
-    
-                # Combina la información si es "Pathogenic" o "Likely pathogenic" en alguno de los dos
-                if (intervar_info and intervar_info["Classification"] in ["Pathogenic", "Likely pathogenic"]) or (clinvar_info and clinvar_info["ClinicalSignificance"] in ["Pathogenic", "Likely pathogenic"]):
-                    combined_results[variant_key] = {
-                        "Gene": clinvar_info["Gene"],
-                        "Genotype": intervar_info["GT"],
-                        "rs": intervar_info["Rs"] if intervar_info["Rs"] != '.' else clinvar_info["rs"],
-                        "IntervarClassification": intervar_info["Classification"],
-                        "ClinvarClinicalSignificance": clinvar_info["ClinicalSignificance"],
-                        "ReviewStatus": clinvar_info["ReviewStatus"],
-                        "ClinvarID": clinvar_info["ClinvarID"],
-                        "Orpha": intervar_info["Orpha"]
-                }
-    except Exception as e:
-        raise Exception(f"Error al combinar resultados: {e}")
-    
-    return(combined_results)
-    
-def write_combined_results_to_tsv(combined_results, vcf_path, category):
-    """
-    Escribe los resultados combinados en un archivo TSV.
-
-    Args:
-        combined_results (dict): Diccionario con los resultados combinados.
-        vcf_path (str): Ruta al archivo VCF normalizado.
-        category (str): Categoría de genes para la anotación.
-    """
-    try:
-        # Abrir el archivo TSV para escritura
-        output_tsv = vcf_path.split('normalized')[0] + category + '_combinedresults.tsv'
-        with open(output_tsv, "w", newline="") as tsv_file:
-            fieldnames = ["Variant", "Gene", "Genotype", "rs", "IntervarClassification", "ClinvarClinicalSignificance", "ReviewStatus", "ClinvarID", "Orpha"]
-            writer = csv.DictWriter(tsv_file, fieldnames=fieldnames, delimiter="\t")
-        
-            # Escribir el encabezado del archivo TSV
-            writer.writeheader()
-        
-            # Iterar sobre las claves (variantes) en el diccionario result
-            for variant, info in combined_results.items():
-                # Asegurarse de que el diccionario 'info' tenga todas las claves necesarias
-                row = {
-                    "Variant": variant,
-                    "Gene": info.get("Gene", ""),
-                    "Genotype": info.get("Genotype", ""),
-                    "rs": info.get("rs", ""),
-                    "IntervarClassification": info.get("IntervarClassification", ""),
-                    "ClinvarClinicalSignificance": info.get("ClinvarClinicalSignificance", ""),
-                    "ReviewStatus": info.get("ReviewStatus", ""),
-                    "ClinvarID": info.get("ClinvarID", ""),
-                    "Orpha": info.get("Orpha", "")
-                }
-    
-                # Escribir la fila en el archivo TSV
-                writer.writerow(row)    
-     
-    except Exception as e:
-        raise Exception(f"Error al escribir resultados en archivo TSV: {e}")
-
-def run_reproductive_risk_module(vcf_path, assembly, mode, evidence_level, category, clinvar_path): #sobra category, este modulo es especifico de rr
-    """
-    Ejecuta el módulo de riesgo personal según el modo seleccionado.
+    Ejecuta el módulo de riesgo farmacogenético.
     
     Args:
+        dir_path (str)
         vcf_path (str): Ruta al archivo VCF de entrada.
         assembly (str): Ensamblaje genómico a utilizar.
-        mode (str): Modo de ejecución ("basic" o "advanced").
-        evidence_level (int): Nivel de evidencia deseado.
-        category (str): Categoría de genes para la anotación.
-        clinvar_path (str): Ruta al archivo de base de datos de CLINVAR.
     """
-    if mode == "basic":
-        run_intervar(vcf_path, output_dir, category, assembly)
-        intervar_results = parse_intervar_output(output_dir, category, mode)
-        return(intervar_results)
-        #check_inheritance()
-    elif mode == "advanced":
-        run_intervar(vcf_path, output_dir, category, assembly)
-        intervar_results = parse_intervar_output(vcf_path, category, mode)
-        clinvar_dct = run_clinvar_filtering(evidence_level, clinvar_path)
-        combined_results = combine_results(vcf_path, category, intervar_results, clinvar_dct)
-        write_combined_results_to_tsv(combined_results, vcf_path, category)
-        return(combined_results)
-        #check_inheritance()
-    else:
-        print("Modo no válido. Elija 'basic' o 'advanced'.")
-        return None
+    # Anotar variantes fg presentes en el vcf
+    fg_results = annotate_fg_variants(dir_path, vcf_path, assembly)
+    
+    # Asignar los diplotipos de cada gen
+    cyp2c19_diplotype = assign_cyp2c9_diplotype(fg_results)
+    dpyd_diplotype = assign_dpyd_diplotype(fg_results)
+    nudt15_diplotype = assign_nudt15_diplotype(fg_results)
+    tpmt_diplotype = assign_tpmt_diplotype(fg_results)
+    
+
